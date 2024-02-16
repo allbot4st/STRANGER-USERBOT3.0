530a22f42146831b7172626b6ec0d9d301eef8c0
allbot4st <160174832+allbot4st@users.noreply.github.com>
2024-02-16 19:55:31 +0530

    Update __init__.py

--- SpamX/modules/__init__.py
+++ SpamX/modules/__init__.py
@@ -8,9 +8,9 @@ import platform
 __version__ = __version__
 
 
-ping_msg = PING_MSG if PING_MSG else "⚡️𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥⚡️"
-pic = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/aa4bf1e57d11fb75b602e.jpg"
-amsg = ALIVE_MSG if ALIVE_MSG else"⚡️𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 𝗦𝗣𝗔𝗠⚡️"
+ping_msg = PING_MSG if PING_MSG else "4sᴛ 𝐎ғғɪᴄɪᴀʟ"
+pic = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/f5b0e7f2bd95153968e87.jpg"
+amsg = ALIVE_MSG if ALIVE_MSG else"𝐆ᴇᴛ 𝐑ᴇᴀᴅʏ 𝐓ᴏ 𝐅ᴜᴄᴋ 𝐁ʏ #_4sᴛ"
 
 try:
    sah = RiZoeL.get_users(OWNER_ID)
@@ -22,16 +22,16 @@ class Alive:
      Pic = pic
      
      msg = f"""
-      **[⚡️𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥⚡️](https://t.me/SHIVANSH474)
+      **[👻4sᴛ 𝐎ғғɪᴄɪᴀʟ💘](https://t.me/I_M_FIGHTER)
 ◈ •━━━━━★✦♡✦★━━━━━• ◈ 
 
-➪ **𝗠ᴀsᴛᴇʀ:** [⚡️𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛⚡️](https://t.me/ITSZ_SHIVANSH)
+➪ **👑4sᴛ 𝐅ʏᴛᴇʀ💌:** [𝐓ᴇʀᴀ 𝐁ᴀᴀᴘ #_4sᴛ](https://t.me/ll4st_MIND_GAMERII)
 
-➪ **𝗣ʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ:**`{platform.python_version()}`
+➪ **❤𝐏ʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ👀:**`{platform.python_version()}`
 
-➪ **𝗧ᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ:** `{__version__}`
+➪ **💞𝐓ᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ:💢** `{__version__}`
 
-➪ **𝗣ʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ:** `{pyro_vr}`
+➪ **💌𝐏ʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ💤:** `{pyro_vr}`
 ◈ •━━━━━★✦♡✦★━━━━━• ◈
      """
 
