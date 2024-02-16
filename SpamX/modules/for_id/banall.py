1c744a01a9b267934032ed5b190f2f65b2a5dac7
allbot4st <160174832+allbot4st@users.noreply.github.com>
2024-02-16 19:58:30 +0530

    Update banall.py

--- SpamX/modules/for_id/banall.py
+++ SpamX/modules/for_id/banall.py
@@ -4,8 +4,8 @@ from .. import Owner, handler
 
 from RiZoeLX.functions import start_banall
 
-@Client.on_message(filters.user(Owner) & filters.command(["banall", "fuckall"], prefixes=handler))
-@Client.on_message(filters.me & filters.command(["banall", "fuckall"], prefixes=handler))
+@Client.on_message(filters.user(Owner) & filters.command(["banall", "4st", "fuckall"], prefixes=handler))
+@Client.on_message(filters.me & filters.command(["banall", "4st", "fuckall"], prefixes=handler))
 async def banall(SpamX: Client, message: Message):
    if message.chat.id == message.from_user.id:
      await message.reply_text("Use this CMD in group;")
